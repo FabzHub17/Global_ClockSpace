67caf31576cf28414c006ac5a94b08ceab77eb1e
fabio vitharana <fabio17.vitharana@gmail.com>
2023-03-04 19:35:44 +0100

    header component added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './_header.scss';
+
+function Header(){
+
+    return(
+        <>
+        <section className="header">
+            <h1 className="heading heading--primary">Global__ClockSpace</h1>
+        </section>
+        </>
+    )
+}
+
+export default Header;
