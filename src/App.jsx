9872cf0aa52b7a1fed28996597d8909ce6bb97f6
fabio vitharana <fabio17.vitharana@gmail.com>
2023-03-05 14:44:32 +0100

    styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
+import MainSection from './components/Header/MainSection/MainSection'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <MainSection />
     </div>
   )
 }
