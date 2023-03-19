3aa25f923c767df7ea00eb58ee648143ae80968c
Fabio <fabio17.vitharana@gmail.com>
2023-03-19 19:16:50 +0100

    fixed path for component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
-import MainSection from './components/Header/MainSection/MainSection'
+import MainSection from './components/MainSection/MainSection'
 
 function App() {
 
