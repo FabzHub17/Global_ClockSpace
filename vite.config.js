3d22908b5a6b2bdfc237bbcf4ff6212ea6325c95
Fabio <fabio17.vitharana@gmail.com>
2023-03-19 19:33:58 +0100

    usePolling:true for hot module reload only needed when running in wls/wls2 terminal

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  //server needs only for fixing hot modulereload in wls2, for normal terminal this actually makes it slower
+  // so if not using wls2, delete this server field.
+  // server:{
+  //   watch: {
+  //     usePolling:true
+  //   }
+  //}
+
 })
